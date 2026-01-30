19dc56b78b7a60f8c8732330ccf2ddb43085e706
Tejas Priyadarshi <tejas@billwithwillow.com>
2026-01-29 23:15:53 -0800

    Update Glue to 2.0.8 - Fix CLI version reading

--- Casks/glue.rb
+++ Casks/glue.rb
@@ -1,6 +1,6 @@
 cask "glue" do
-  version "2.0.7"
-  sha256 "11647561d6a331217b68fa19b3bb912910ac3dfdc61549c55369bfd23824f1aa"
+  version "2.0.8"
+  sha256 "abb3bb79369435f3b02845c6eeb60ddc0d83d234a7b572b915ae678175ff65a1"
 
   url "https://github.com/billwithwillow/glue-releases/releases/download/v#{version}/Glue-#{version}-mac.zip"
   name "Glue"
