893ae9663ba8a7aa8c4010cbc3a52a33792534c1
Tejas Priyadarshi <tejas@billwithwillow.com>
2026-02-13 14:41:50 -0800

    Fix: use hyphenated Glue-Beta in artifact URL

--- Casks/glue-beta.rb
+++ Casks/glue-beta.rb
@@ -2,7 +2,7 @@ cask "glue-beta" do
   version "2.17.2-beta.1"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-  url "https://github.com/billwithwillow/glue-releases/releases/download/v#{version}/Glue%20Beta-#{version}-mac.zip"
+  url "https://github.com/billwithwillow/glue-releases/releases/download/v#{version}/Glue-Beta-#{version}-mac.zip"
   name "Glue Beta"
   desc "Dev-only UI annotation tool with source-linked element IDs (beta channel)"
   homepage "https://github.com/billwithwillow/glue"
